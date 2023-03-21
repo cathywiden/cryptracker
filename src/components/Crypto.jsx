deb2bd61d6ef29972aa052c823b114d7eabfb6fa
Katalin Widén <katalinwiden@gmail.com>
2023-03-21 20:45:09 +0100

    minor fixes
    check for valid data entered (amount && date && format)
    minor formatting fixes

--- src/components/Crypto.jsx
+++ src/components/Crypto.jsx
@@ -1,15 +1,63 @@
 import React from "react";
 
 const Crypto = ({ name, transactions, addTransaction, deleteTransaction }) => {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const transaction = {
-      amount: event.target.amount.value,
-      date: event.target.date.value,
-    };
-    addTransaction(name, transaction);
-    event.target.reset();
+const handleSubmit = (event) => {
+  event.preventDefault();
+
+  // Check if amount and date are filled in
+  const amount = event.target.amount.value.trim();
+  const date = event.target.date.value.trim();
+
+  if (amount === "" || date === "") {
+    alert("Please fill in both amount and date.");
+    return;
+  }
+
+  // Check if amount is greater than zero
+  if (parseFloat(amount) <= 0) {
+    alert("Please enter an amount greater than zero.");
+    return;
+  }
+
+  // Check if date is in a valid format (dd/mm/yyyy)
+  const dateRegex = /^(\d{2})\/(\d{2})\/(\d{4})$/;
+  const matches = date.match(dateRegex);
+  if (!matches) {
+    alert("Please enter a valid date in the format dd/mm/yyyy.");
+    return;
+  }
+
+  // Check if date is valid
+  const day = parseInt(matches[1]);
+  const month = parseInt(matches[2]) - 1;
+  const year = parseInt(matches[3]);
+  const inputDate = new Date(year, month, day);
+  const currentDate = new Date();
+  if (
+    inputDate.getDate() !== day ||
+    inputDate.getMonth() !== month ||
+    inputDate.getFullYear() !== year
+  ) {
+    alert("Please enter a valid date.");
+    return;
+  }
+
+  // Check if date is not in the future
+  if (inputDate > currentDate) {
+    alert("Please enter a date that is not in the future.");
+    return;
+  }
+
+  const transaction = {
+    amount: amount,
+    date: date,
   };
+  addTransaction(name, transaction);
+  event.target.reset();
+};
+
+
+
 
   const handleDelete = (index) => {
     const updatedTransactions = transactions.filter((_, i) => i !== index);
@@ -40,14 +88,13 @@ const Crypto = ({ name, transactions, addTransaction, deleteTransaction }) => {
           <input
             type="text"
             name="amount"
+            placeholder="Enter amount"
             style={{ marginLeft: "4px", marginRight: "8px" }}
           />
-        </label>
-        <label>
-          Date:
           <input
             type="text"
             name="date"
+            placeholder="Enter date (dd/mm/yyyy)"
             style={{ marginLeft: "4px", marginRight: "8px" }}
           />
         </label>
